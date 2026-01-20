74384402f4533a5b824418344162f2da5127a931
MGLMendes <miguelmendes203@gmail.com>
2026-01-20 17:09:21 -0300

    `5.9` feat: add `Customer`

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.algaworks.algashop.ordering.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+
+    private UUID id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String email;
+    private String phone;
+    private String document;
+    private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Integer loyaltyPoints;
+}
